bd4fc0dde1126a458d7a177e8222e362774ea8ec
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-05-25 00:38:18 +0530

    Update Introduction.java

--- Introduction.java
+++ Introduction.java
@@ -6,7 +6,7 @@ class Introduction {
     public static void main(String[] args) {
         String name = "Riddhiman Bhattacharya";
         int age = 17;
-        String[] interests = {"Physics", "Mathematics", "Coding"};
+        String[] interests = {"Physics", "Mathematics", "Graphing", "Coding", "Cryptography"};
         
         System.out.printf("Hi, my name is %.\n", name);
         System.out.printf("I'm %d years old.\n", age);
@@ -14,7 +14,8 @@ class Introduction {
         for (String interest : interests) {
             System.out.printf("- %s\n", interest);
         }
-        System.out.println("I'm excited to be learning and growing in my programming skills, and I look forward to connecting with others in the community!");
+        System.out.println("I want to be a Cosmologist & look forward to solve the secrets of Cosmos using
+"Mathematics" & "Physics"");
     }
 }
 
